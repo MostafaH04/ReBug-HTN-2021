8b1bf7bcda1bd73b590c38487fffa7500fcd9a6d
Mostafa Hussein <tnt.empire00@gmail.com>
2021-09-19 11:05:04 -0400

    updates

--- storage.py
+++ storage.py
@@ -9,7 +9,15 @@ files = sorted(glob.iglob(path), key=os.path.getctime,reverse=True)
 if files:
   recent = files[0]
 
-config = json.load(open('config.json'))
+config = {
+    "apiKey": "AIzaSyC1DIqvIw_BEdLhWzdlF301BWWmbuNhyhA",
+    "authDomain": "rebug-aecaa.firebaseapp.com",
+    "databaseURL": "https://rebug-aecaa-default-rtdb.firebaseio.com/",
+    "projectId": "rebug-aecaa",
+    "storageBucket": "rebug-aecaa.appspot.com",
+    "serviceAccount": "serviceAccountKey.json",
+    "messagingSenderId": "1057776412856"
+}
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
