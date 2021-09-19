77f5bd62c66587be63ffdb37e6c6696c1e6310e0
Siddharth <siddharthnema04@gmail.com>
2021-09-19 09:39:31 -0400

    more updates

--- storage.py
+++ storage.py
@@ -1,21 +1,15 @@
 import glob
 import os
 import pyrebase
+import json
+
+config = json.load(open('config.json'))
+
+
 path = "uploads/*jpg"
 files = sorted(glob.iglob(path), key=os.path.getctime,reverse=True)
 
 recent = files[0]
-
-print(recent)
-
-config = {
-  'apiKey': "AIzaSyC1DIqvIw_BEdLhWzdlF301BWWmbuNhyhA",
-  'authDomain': "rebug-aecaa.firebaseapp.com",
-  'databaseURL': "https://rebug-aecaa-default-rtdb.firebaseio.com",
-  'projectId': "rebug-aecaa",
-  'storageBucket': "rebug-aecaa.appspot.com",
-  'serviceAccount': "serviceAccountKey.json"
-};
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
